ad4dda92b985d0947c6a1924905c3ce2823a0189
Nikita Bayev <drugoi.nik@gmail.com>
2015-06-04 10:36:01 +0600

    Fixed tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt) {
   require('time-grunt')(grunt);
-  require('jit-grunt')(grunt);
+  require('jit-grunt')(grunt, {
+    'htmlbuild': 'grunt-html-build'
+  });
   require('load-grunt-config')(grunt, {
     jitGrunt: true
   });
